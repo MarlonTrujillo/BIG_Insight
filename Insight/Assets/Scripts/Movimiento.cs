146d827e1465871d09b3d230f59e66702506a614
MarlonTrujillo <marlongecko@gmail.com>
2016-10-24 23:20:38 -0500

    Versión 0.1

--- Insight/Assets/Scripts/Movimiento.cs
+++ Insight/Assets/Scripts/Movimiento.cs
@@ -1,18 +1,66 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Movimiento : MonoBehaviour {
+public class Movimiento : MonoBehaviour
+{
+
+    private CharacterController characterController;
+    public bool isGrounded;
+    public float gravity;
+    private float fallSpeed;
+    public float jumpSpeed;
+    public float moveSpeed;
+    public float value;
 
 	// Use this for initialization
 	void Start () {
-	
+        characterController = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey(KeyCode.D))
+        IsGrounded();
+        Fall();
+        Jump();
+        Move();
+
+    }
+
+    void Move()
+    {
+        float xSpeed = Input.GetAxis("Horizontal");
+        if (xSpeed != 0)
         {
-            this.transform.position += new Vector3(2*Time.deltaTime,0, 0);
+            characterController.Move(new Vector3(xSpeed,0,0)* moveSpeed*Time.deltaTime);
         }
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            fallSpeed = -jumpSpeed;
+        }
+    }
+
+    void Fall() // Método que aplica gravedad al sujeto
+    {
+        if (!isGrounded)
+        {
+            fallSpeed += gravity * Time.deltaTime;
+        }
+        else
+        {
+            if (fallSpeed > 0)
+            {
+                fallSpeed = 0;
+            }
+        }
+        characterController.Move(new Vector3(0, -fallSpeed, 0)*Time.deltaTime);
+    }
+
+    void IsGrounded() // método que detecta si el personaje está en el suelo coliionando
+    {
+        isGrounded = (Physics.Raycast(transform.position, -transform.up, characterController.height / value));
     }
 }
