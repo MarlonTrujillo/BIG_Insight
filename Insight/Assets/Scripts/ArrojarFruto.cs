12177ea777d13dd3a41bf88419d367963436e0cd
MarlonTrujillo <marlongecko@gmail.com>
2016-10-31 23:43:58 -0500

    Versión 0.2 (con checkpoint)

--- Insight/Assets/Scripts/ArrojarFruto.cs
+++ Insight/Assets/Scripts/ArrojarFruto.cs
@@ -12,7 +12,7 @@ public class ArrojarFruto : MonoBehaviour
 	private bool lanzado = false; // Si la bola ha sido lanzada, previene que sean lanzada más de una
 	public GameObject ballClone; // Para uno usar el prefab original
 	private Rigidbody rb; // Variable para usar el RigidBody
-	private int frutosDisponibles = 3; // Cantidad de frutos disponibles en el nivel
+	public int frutosDisponibles = 3; // Cantidad de frutos disponibles en el nivel
 	new Camera camera;
 
 	public void SetLanzado(bool lanzado) // Constructor para acceder
@@ -23,38 +23,49 @@ public class ArrojarFruto : MonoBehaviour
 	// Use this for initialization
 	void Start ()
     {
-		camera = GetComponent<Camera> ();
+		camera = GetComponentInChildren<Camera> ();
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
         Arrojar();
-            
 
 
 
 
 
-        }
 
-        void Arrojar()
-    {
-        Vector3 screenPos = camera.WorldToScreenPoint(GameObject.Find("personaje").transform.position);
-        throwSpeed.x = Input.mousePosition.x - screenPos.x;
-        throwSpeed.y = (Input.mousePosition.y - screenPos.y) * 2;
-        Debug.Log(throwSpeed);
-        if (Input.GetMouseButtonDown(0) && !lanzado && frutosDisponibles > 0) // MouseIzq, no ha lanzado, y quedan frutos
-        {
-            Debug.Log("lanzado");
-            lanzado = true; // Ya se lanzó
-            frutosDisponibles--; // Resta a la cantidad de frutos
-
-            // Crea una instancia del prefab ball desde la posición del personaje y anade rotación
-            ballClone = Instantiate(ball, GameObject.Find("personaje").transform.position, transform.rotation) as GameObject;
-
-            rb = ballClone.GetComponent<Rigidbody>(); // Obtiene el RigidBody del prefab
-            rb.AddForce(throwSpeed / 100, ForceMode.Impulse); // Añade el impulso
-        }
     }
+
+    // Este método arroja la bola
+        void Arrojar()
+        {
+            Vector3 screenPos = camera.WorldToScreenPoint(GameObject.Find("personaje").transform.position);
+            throwSpeed.x = Input.mousePosition.x - screenPos.x;
+            throwSpeed.y = (Input.mousePosition.y - screenPos.y) * 2;
+            Debug.Log(throwSpeed);
+            if (Input.GetMouseButtonDown(0) && !lanzado && frutosDisponibles > 0) // MouseIzq, no ha lanzado, y quedan frutos
+            {
+                Debug.Log("lanzado");
+                lanzado = true; // Ya se lanzó
+                frutosDisponibles--; // Resta a la cantidad de frutos
+
+                // Crea una instancia del prefab ball desde la posición del personaje y anade rotación
+                ballClone = Instantiate(ball, GameObject.Find("personaje").transform.position, transform.rotation) as GameObject;
+
+                rb = ballClone.GetComponent<Rigidbody>(); // Obtiene el RigidBody del prefab
+                rb.AddForce(throwSpeed / 100, ForceMode.Impulse); // Añade el impulso
+            }
+        }
+
+        void OnTriggerEnter(Collider other) // Colisión Trigger
+        {
+            if (other.gameObject.tag == "checkpoint") // Si el gameobject es un ball (tag)
+            {
+                frutosDisponibles += 5;
+            }
+        }
+
+
 }
